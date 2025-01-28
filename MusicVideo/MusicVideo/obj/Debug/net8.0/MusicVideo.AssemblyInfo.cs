331b46f9211279dc97d2cdcdbf03fce6c2bdb870
Rajumary <rajumaryr@gmail.com>
2025-01-27 19:08:57 -0700

    MusicVideo

--- MusicVideo/MusicVideo/obj/Debug/net8.0/MusicVideo.AssemblyInfo.cs
+++ MusicVideo/MusicVideo/obj/Debug/net8.0/MusicVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4024384b38ac1de0a95d0bc70e5b42651f43e99b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6bbf4cb7566eef466ce9f6cfe83c583a68067c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
